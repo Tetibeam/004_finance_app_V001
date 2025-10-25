9ad35941115da1df55c84294efb3e474528a98c6
unknown <itecchi01@gmail.com>
2025-10-25 21:37:07 +0900

    ダッシュボード

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from utils.config import load_settings
 import utils.visualize_dashboard as viz
 from utils.calculation import cal_general_special_balance_dashboard
 import os
+import plotly.io as pio
 
 settings = load_settings("setting.yaml")
 DB_PATH_FINANCE = os.path.join(    settings["database_path"],"",settings["database"]["finance"])
@@ -21,28 +22,28 @@ def dashboard():
     df_asset_profit = get_asset_and_profit_dashboard(DB_PATH_FINANCE) # DBから資産データを取得、整形
     # 1. 総資産推移
     fig = viz.display_total_assets(df_asset_profit)
-    graphs["assets"] = fig.to_html(full_html=False)
+    graphs["assets"] = pio.to_html(fig, full_html=False, include_plotlyjs='cdn')
     
     # 2. トータルリターン推移
     fig = viz.display_total_returns(df_asset_profit)
-    graphs["returns"] = fig.to_html(full_html=False)
+    graphs["returns"] = pio.to_html(fig, full_html=False, include_plotlyjs='cdn')
     
     df_balance = get_balance_dashboard(DB_PATH_FINANCE)
     df_general = cal_general_special_balance_dashboard(df_balance, "一般収支")
     df_special = cal_general_special_balance_dashboard(df_balance, "特別収支")
     # 3. 一般収入・支出
     fig = viz.display_general_income_expenditure(df_general)
-    graphs["general_income_expenditure"] = fig.to_html(full_html=False)
+    graphs["general_income_expenditure"] = pio.to_html(fig, full_html=False, include_plotlyjs='cdn')
     # 4. 一般収支
     fig = viz.display_general_balance(df_general)
-    graphs["general_balance"] = fig.to_html(full_html=False)
+    graphs["general_balance"] = pio.to_html(fig, full_html=False, include_plotlyjs='cdn')
     # 5. 特別収入・支出
     fig = viz.display_special_income_expenditure(df_special)
-    graphs["special_income_expenditure"] = fig.to_html(full_html=False)
+    graphs["special_income_expenditure"] = pio.to_html(fig, full_html=False, include_plotlyjs='cdn')
     # 6. 特別収支
     fig = viz.display_special_balance(df_special)
-    graphs["special_balance"] = fig.to_html(full_html=False)
-
+    #graphs["special_balance"] = fig.to_html(full_html=False)
+    graphs["special_balance"] = pio.to_html(fig, full_html=False, include_plotlyjs='cdn')
     return render_template("dashboard.html", graphs=graphs)
     """
     pivot = get_total_assets(db_path=DB_PATH_ASSET).sum(axis=1)
--- utils/visualize_dashboard.py
+++ utils/visualize_dashboard.py
@@ -3,25 +3,46 @@ import pandas as pd
 
 def common_setting(fig):
     fig.update_xaxes(tickformat="%y/%m/%d")
-    fig.update_layout(autosize=True, margin=dict(l=10,r=10,t=50,b=20))
+    fig.update_layout(
+        autosize=True,
+        width=290,
+        height=234,
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        margin=dict(l=0,r=0,t=50,b=20),
+        title_font=dict(size=12),            # タイトル文字サイズ
+        font=dict(size=8),                  # 軸ラベル・凡例などの共通サイズ
+        legend=dict(
+            visible=True,
+            orientation="h",
+            yanchor="bottom",
+            y=1.1,
+            xanchor="center",
+            x=0.75,
+            font=dict(size=8),
+            #tracegroupgap=5,  # trace間のギャップ
+        )
+    )
     return fig
 
 def display_total_assets(df):
-    fig = px.line(df, x=df.index, y=["資産額_実績", "資産額_目標"],
-              title="総資産推移", labels={"date": "日付", "values":"資産額","variable":""}, template="plotly_dark")
+    fig = px.line(
+        df, x=df.index, y=["資産額_実績", "資産額_目標"],template="plotly_dark",title="総資産推移",
+        labels={"date": "日付", "value":"資産額","variable":""}
+        )
     fig = common_setting(fig)
     return fig
 
 def display_total_returns(df):
     fig = px.line(df, x=df.index, y=["トータルリターン_実績", "トータルリターン_目標"],
-              title="トータルリターン推移", labels={"date": "日付", "values":"リターン額","variable":""}, template="plotly_dark")
+              title="トータルリターン推移", labels={"date": "日付", "value":"リターン額","variable":""}, template="plotly_dark")
     fig = common_setting(fig)
     return fig
 
 def display_general_income_expenditure(df):
     fig = px.bar(
         df, x=df.index, y=["金額_収入", "金額_支出"],barmode='group', template='plotly_dark',
-        labels={'value':'金額', 'date':'年月', 'variable':'種別'}, title = "一般収入・支出")
+        labels={'value':'金額', 'date':'年月', 'variable':''}, title = "一般収入・支出")
     #fig.show()
     fig.add_scatter(
         x=df.index,
@@ -59,7 +80,7 @@ def display_general_balance(df):
 def display_special_income_expenditure(df):
     fig = px.bar(
         df, x=df.index, y=["金額_収入", "金額_支出"],barmode='group', template='plotly_dark',
-        labels={'value':'金額', 'date':'年月', 'variable':'種別'}, title = "特別収入・支出")
+        labels={'value':'金額', 'date':'年月', 'variable':''}, title = "特別収入・支出")
 
     fig.add_scatter(
         x=df.index,
